de716f020dea74f566a2ca899844cd54e8801471
Muhammad Safwan P K <safwanmuhammad3099@gmail.com>
2025-09-27 19:23:46 +0300

    setup done

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <Button variant={"outline"}>hello from nextjs</Button>
+    <div></div>
 
   );
 }
--- components/ui/button.jsx
+++ components/ui/button.jsx
@@ -19,11 +19,13 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
+        primary:"bg-gradient-to-r from-purple-600 to-blue-600 hover:from-purple-700 hover:to-blue-700 shadow-xs"
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
         sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
         lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
+        xl: "h-12 rounded-md px-6 has-[>svg]:px-4",
         icon: "size-9",
       },
     },
